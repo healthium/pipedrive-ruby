fada0eb0eca84710f01804b2d8f5234c3e86ec39
Diogo Alves <diogo.alves.bcl@gmail.com>
2020-06-17 20:28:49 +0100

    Update pipeline deals method

--- lib/pipedrive/pipeline.rb
+++ lib/pipedrive/pipeline.rb
@@ -1,7 +1,7 @@
 module Pipedrive
   class Pipeline < Base
     def stages
-      Stage.all(query: { pipeline_id: self.id })
+      Stage.all(query: { pipeline_id: id })
     end
 
     def statistics(id, start_date, end_date)
@@ -11,8 +11,8 @@ module Pipedrive
       res.ok? ? new(res) : bad_response(res,{ :id => id, :start_date => start_date, :end_date => end_date })
     end
 
-    def deals(id, stage_id)
-      Pipedrive::Deal.all(request_path: "#{resource_path}/#{id}/deals", query: { :stage_id => stage_id })
+    def deals(options = {})
+      Deal.all(options.merge(request_path: build_request_path(:deals)))
     end
   end
 end
